a0982b041d821ef9617748fb71978a2b7b431c47
juneaufig <151437031+juneaufig@users.noreply.github.com>
2023-12-08 13:22:25 +0000

    Update Clue.java

--- Clue.java
+++ Clue.java
@@ -1,5 +1,53 @@
 public class Clue {
     public static void main(String[] args) {
-        
+
     }
+
+    /**
+     * @param elements
+     */
+    public Clue(LetterStatus[] elements) {
+
+    }
+
+    /**
+     * @param orderNumber
+     * @param wordSize
+     */
+    public Clue(int orderNumber, int wordSize) {
+
+    }
+
+    /**
+     * @return
+     */
+    public int length() {
+
+    }
+
+    /**
+     * @return
+     */
+    public int orderNumber() {
+
+    }
+
+    /**
+     * @return
+     */
+    public LetterStatus[] letterStatus() {
+
+    } 
+
+    /**
+     * @return
+     */
+    public boolean isMax() {
+
+    }
+
+    public String toString() {
+
+    }
+
 }
