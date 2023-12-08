2f185c40ca22742c45210b0a0991c1d464d0e9d7
juneaufig <151437031+juneaufig@users.noreply.github.com>
2023-12-08 13:26:39 +0000

    Update IpurdleGame.java

--- IpurdleGame.java
+++ IpurdleGame.java
@@ -2,4 +2,65 @@ public class IpurdleGame {
     public static void main(String[] args) {
         
     }
+
+    /**
+     * @param wordSize
+     * @param maxGuesses
+     */
+    public IpurdleGame(int wordSize, int maxGuesses) {
+
+    }
+
+    /**
+     * @return
+     */
+    public int wordLength() {
+
+    }
+
+    /**
+     * @return
+     */
+    public int maxGuesses() {
+
+    }
+
+    /**
+     * @return
+     */
+    public int guesses() {
+
+    }
+
+    /**
+     * @param guess
+     * @return
+     */
+    public boolean isValid(String guess) {
+
+    }
+
+    /**
+     * @return
+     */
+    public boolean isOver() {
+
+    }
+
+    /**
+     * @param guess
+     * @param word
+     * @return
+     */
+    private Clue clueForGuessAndWord(String guess, String word) {
+
+    }
+
+    /**
+     * @param guess
+     * @return
+     */
+    public Clue playGuess(String guess) {
+
+    }
 }
