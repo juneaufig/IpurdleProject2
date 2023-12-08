eaeaba5ebc37d912764f1f6b891030390cf87795
juneaufig <151437031+juneaufig@users.noreply.github.com>
2023-12-08 13:21:46 +0000

    Update Board.java

--- Board.java
+++ Board.java
@@ -2,4 +2,45 @@ public class Board {
     public static void main(String[] args) {
         
     }
+
+    /**
+     * @param wordSize
+     * @param maxGuesses
+     */
+    public Board(int wordSize, int maxGuesses) {
+
+    }
+
+    /**
+     * @return
+     */
+    public int wordLength() {
+
+    }
+
+    /**
+     * @return
+     */
+    public int maxGuesses() {
+
+    }
+
+    /**
+     * @return
+     */
+    public int guesses() {
+
+    }
+
+    /**
+     * @param guess
+     * @param clue
+     */
+    public void insertGuessAndClue(String guess, Clue clue) {
+
+    }
+
+    public String toString() {
+
+    }
 }
