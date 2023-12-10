21a30b3ea74289e18befc5f0775e744a61aa63aa
Dieniss <153366465+Dieniss@users.noreply.github.com>
2023-12-10 16:12:33 +0000

    Board.java

--- Board.java
+++ Board.java
@@ -1,35 +1,43 @@
 public class Board {
-    public static void main(String[] args) {
-        
-    }
+    private char[] word;
+    private int maxGuesses;
+    private int guesses;
+    private String guess;
+    private Clue clue;
 
     /**
      * @param wordSize
      * @param maxGuesses
      */
     public Board(int wordSize, int maxGuesses) {
-
+        if(wordSize>=1 && maxGuesses >= 1){
+            this.word = new char[wordSize];
+            this.guesses = 0;
+            this.maxGuesses = maxGuesses;
+        }else if(wordSize<1 || maxGuesses < 1){
+            System.out.println("Word size and max Guesses must be greater than 1.");
+        }
     }
 
     /**
      * @return
      */
     public int wordLength() {
-
+        return word.length;
     }
 
     /**
      * @return
      */
     public int maxGuesses() {
-
+        return this.maxGuesses;
     }
 
     /**
      * @return
      */
     public int guesses() {
-
+        return this.guesses;
     }
 
     /**
@@ -37,10 +45,17 @@ public class Board {
      * @param clue
      */
     public void insertGuessAndClue(String guess, Clue clue) {
-
+        this.guess = guess;
+        this.clue = clue;
+        this.guesses += 1;
     }
 
     public String toString() {
+        StringBuilder str = new StringBuilder();
+        str.append("+-").append("-".repeat(wordLength()*2+3)).append("-+\n");
+        str.append("| ").append(guess).append(" | ").append(clue.toString()).append(" |\n");
+        str.append("+-").append("-".repeat(wordLength()*2 + 3)).append("-+\n");
 
+        return str.toString();
     }
 }
