3ed5ff547aacf590fb00a7b60b1a751b2071be77
Dieniss <153366465+Dieniss@users.noreply.github.com>
2023-12-17 15:40:39 +0000

    Board.java

--- Board.java
+++ Board.java
@@ -8,8 +8,11 @@ public class Board {
     private ArrayList<Clue> clues = new ArrayList<Clue>();
 
     /**
-     * @param wordSize
-     * @param maxGuesses
+     * Creates a empty Board where the game Ipurdle is going to run.
+     * 
+     * @param wordSize size of words in this game.
+     * @param maxGuesses maximum amount of guesses allowed.
+     * @requires {@code wordSize} and {@code maxGuesses} to be greater or equal to 1.
      */
     public Board(int wordSize, int maxGuesses) {
         if(wordSize>=1 && maxGuesses >= 1){
@@ -22,35 +25,38 @@ public class Board {
     }
 
     /**
-     * @return
+     * @return size of the word to guess.
      */
     public int wordLength() {
         return word.length;
     }
 
     /**
-     * @return
+     * @return the maximum amount of guesses allowed.
      */
     public int maxGuesses() {
         return this.maxGuesses;
     }
 
     /**
-     * @return
+     * @return number of guesses.
      */
     public int guesses() {
         return guesses = guesslist.size();
     }
 
     /**
-     * @param guess
-     * @param clue
+     * Stores the inputed {@code guess} and {@code clue} in their respective array lists.
+     * @param guess word input by player in this round of game.
+     * @param clue symbolic representation of characters correct, incorrect, or in wrong position in word.
      */
     public void insertGuessAndClue(String guess, Clue clue) {
         guesslist.add(guess);
         clues.add(clue);
     }
-
+    /**
+     * Creates a graphical representation of the game being run showing the {@code guess} on the left and the matching {@code clue}.
+     */
     public String toString() {
         StringBuilder str = new StringBuilder();
         str.append("+-").append("-".repeat(wordLength()*2+3)).append("-+\n");
